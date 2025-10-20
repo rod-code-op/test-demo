3941befc39ab284e677e01206dd3fd26dd0c2b92
rod-code-op <rodsilvano18@gmail.com>
2025-10-21 00:05:02 +0800

    add hotel homepage

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -4,15 +4,15 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 final class HomeController extends AbstractController
 {
-    #[Route('/home', name: 'app_home')]
+    #[Route('/', name: 'home')]
     public function index(): Response
     {
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'message' => 'Welcome to My Symfony App!',
         ]);
     }
 }
